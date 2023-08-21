4279d47108f083ed01735f6e2b27854306dba5a1
David Ahijevych <ahijevyc@ucar.edu>
2023-08-21 11:58:15 -0600

    Use config file (#1)
    * Use config file for common variables.
    
    Define mesh_dict, dx, and datadir.
    
    Use os.path.splitext to insert subtring between base filename and
    extension.
    
    * note about defining associative array mesh_dict

--- create_connectivity-file.sh
+++ create_connectivity-file.sh
@@ -7,25 +7,12 @@
 # This script generates a grid connectivity file using the 'GenerateConnectivityFile' command.
 
 # Instructions:
-# 1. Modify the 'mesh_dict' associative array to map resolution names to the corresponding mesh IDs.
-# 2. Specify the desired resolution by setting the 'dx' variable to the desired resolution.
-# 3. Set the 'scripfile' variable to the path of the SCRIP file.
-# 4. Set the 'gridconnectivityfile' variable to the desired path for the grid connectivity file.
-# 5. Run this script to generate the grid connectivity file.
+# 1. Set the 'scripfile' variable to the path of the SCRIP file.
+# 2. Set the 'gridconnectivityfile' variable to the desired path for the grid connectivity file.
+# 3. Run this script to generate the grid connectivity file.
 
-# Associative array mapping resolution names to mesh IDs
-declare -A mesh_dict
-mesh_dict["480km"]=2562
-mesh_dict["240km"]=10242
-mesh_dict["120km"]=40962
-mesh_dict["60km"]=163842
-mesh_dict["30km"]=655362
-mesh_dict["15km"]=2621442
-mesh_dict["7.5km"]=10458762
-mesh_dict["3.75km"]=41943042
-
-# Specify the desired resolution for grid connectivity generation
-dx="15km"
+# Define dx, datadir, and mesh_dict.
+. config.sh
 
 # Path to the SCRIP file
 scripfile="/glade/scratch/fjudt/mpas_meshes/x1.${mesh_dict[$dx]}.grid.SCRIP.nc"
